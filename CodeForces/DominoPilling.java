210d2c9e5466aca883e79d9cd1093409d2a2ec2a
Amine2000s <92222814+Amine2000s@users.noreply.github.com>
2024-07-09 06:52:02 +0100

    Domino pilling

---
+++ CodeForces/DominoPilling.java
@@ -0,0 +1,17 @@
+
+public class DominoPilling {
+    public static void main(String[] args) {
+        int n, m ;
+        Scanner in = new Scanner(System.in);
+
+        n = in.nextInt();
+        m = in.nextInt();
+
+        int result = n * m  ;
+
+        System.out.println(result / 2 );
+
+
+    }
+
+}
